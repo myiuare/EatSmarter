1276d1c5f953a5210603b8a66394adfab64a8768
Halcyo-rgb <sdoufi25@gmail.com>
2025-04-30 16:28:44 +0200

    design

--- header.php
+++ header.php
@@ -1,10 +1,21 @@
 <header>
 
-<nav>
+<link rel="stylesheet" href="style.css">
+
+<nav class="menu-container">
+   <span></span>
+   <span></span>
+   <span></span>
+  <!-- menu items -->
+  <div class="menu">
     <ul>
-        <li><a href="Menu4.0.php">Menu</a></li>
-        <li><a href="me_contacter.php">Me Contacter</a></li>
+      <li><a href="Index.php">Accueil</a></li>
+      <li><a href="Menu 5.0.php">Menu</a></li>
+      <li><a href="commander.php">Commander</a></li>
+      <li><a href="a_propos.php">À propos</a></li>
+      <li><a href="me_contacter.php">Me Contacter</a></li>
     </ul>
+  </div>
 </nav>
 
 </header>
